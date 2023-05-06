a083c4406d60c711fa7d97ffa15a10d8f22d9364
Robert Starmer <rstarmer@gmail.com>
2023-05-06 15:37:08 -0700

    Fix root path and run.sh
    root path can't be defined in variables so I've updated http_directory
    run.sh needed build before -var callout

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-PACKER_LOG=1 packer -var ssh_password=${SSH_PASSWORD} build bullseye.pkr.hcl
+PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} bullseye.pkr.hcl
