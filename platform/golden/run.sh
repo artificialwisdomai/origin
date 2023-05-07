97834a733b1e237d1bf8452a5b4224e15e7ab0f0
Robert Starmer <rstarmer@gmail.com>
2023-05-07 01:01:59 -0700

    Fixed secrets and sudo
    Fixed ssh/build password by passing preseed cfg
      parameters via boot kernel parameters
    Removed late_command for sudo (changed user group settings)

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} bullseye.pkr.hcl
+BUILD_TIME=$(date +%Y-%m-%d-%H-%M-%S)
+PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} -var build_time=${BUILD_TIME} bullseye.pkr.hcl
