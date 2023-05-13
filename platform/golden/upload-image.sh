5b960d8febcbdda4dffacaf46175d514c450863f
Robert Starmer <robert@computelify.com>
2023-05-13 00:57:33 -0700

    upload script now waits for import

--- platform/golden/upload-image.sh
+++ platform/golden/upload-image.sh
@@ -1,15 +1,28 @@
 cd build
-if ! $(oci os bucket list | grep debian-golden >& /dev/null)
-then
+if [ $(oci os bucket list | jq '.data[0].name' | grep "debian-golden" | wc -l) -ge 1 ] ; then
+  echo "bucket debian-golden exists"
+else
   oci os bucket create --bucket-name debian-golden
+  if [ $? -gt 0 ]; then exit 1; fi
 fi
-if -f golden.raw-disk001.vmdk
-then
-  oci os object put --bucket-name debian-golden --file golden.raw-disk001.vmdk
+if [ -f golden.raw-disk001.vmdk ]; then
+  oci os object put --bucket-name debian-golden --file golden.raw-disk001.vmdk --force
+  if [ $? -gt 0 ]; then exit 1; fi
 else
   echo no vmdk to upload!
 fi
-oci compute image import from-object --namespace $(oci os ns get | jq .data | tr -d '"') \
- --bucket-name debian-golden --name golden.raw-disk001.vmdk > oci-import.out
+NAMESPACE=$(oci os ns get | jq .data | tr -d '"')
+WORK_REQUEST=$(oci compute image import from-object --namespace $NAMESPACE \
+	--launch-mode PARAVIRTUALIZED --display-name debian-golden \
+	--bucket-name debian-golden --name golden.raw-disk001.vmdk )
+echo $WORK_REQUEST
+WORK_REQUEST_ID=$(echo $WORK_REQUEST | jq '."opc-work-request-id"')
+echo $WORK_REQUEST_ID
+WORK_DISPLAY_NAME=$(echo $WORK_REQUEST | jq '.data."display-name"')
+echo $WORK_DISPLAY_NAME
+if [ $? -gt 0 ]; then exit 1; fi
+while [ "$(oci work-requests work-request get --work-request-id $WORK_REQUEST_ID \
+	| jq '.data.status')" != "SUCCESS" ]; do echo waiting; sleep 90; done
+echo "SUCCESS importing $WORK_DISPLAY_NAME"
 
 cd ..
