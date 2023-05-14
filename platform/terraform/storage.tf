3b988be50de61b5155d839bcdd69e25b8b123f7b
Robert Starmer <rstarmer@gmail.com>
2023-05-14 08:30:00 -0700

    Update storage to point to vmdk

--- platform/terraform/storage.tf
+++ platform/terraform/storage.tf
@@ -25,3 +25,19 @@ resource "oci_objectstorage_object" "debian_golden" {
 #     source_uri = oci_objectstorage_bucket.debian_golden.namespace_path
 #   }
 # }
+
+resource "oci_core_image" "debian_golden" {
+    #Required
+    compartment_id = var.compartment_id
+
+    #Optional
+    display_name = var.image_display_name
+    launch_mode = var.image_launch_mode
+
+    image_source_details {
+        source_type = "objectStorageTuple"
+        bucket_name = var.bucket_name
+        namespace_name = var.namespace
+        object_name = oci_objectstorage_object.debian_golden.object # exported image name
+    }
+}
