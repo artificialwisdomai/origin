a6b5f08943c70822d7e9298e5913f1f4ba40efe6
Robert Starmer <rstarmer@gmail.com>
2023-05-14 08:39:25 -0600

    Added storage upload and image create

--- platform/terraform/storage.tf
+++ platform/terraform/storage.tf
@@ -2,14 +2,14 @@ resource "oci_objectstorage_bucket" "debian_golden" {
   # Bucket configurations
   compartment_id = var.compartment_id
   name           = "debian-golden"
-  namespace      = var.oci_namespace
+  namespace      = var.namespace
 }
 
 resource "oci_objectstorage_object" "debian_golden" {
   bucket    = oci_objectstorage_bucket.debian_golden.name
-  source    = "build/golden.raw-disk001.vmdk"
+  source    = "golden.raw-disk001.vmdk"
   object    = "golden.raw-disk001.vmdk"
-  namespace = var.oci_namespace
+  namespace = var.namespace
 }
 
 
@@ -25,3 +25,19 @@ resource "oci_objectstorage_object" "debian_golden" {
 #     source_uri = oci_objectstorage_bucket.debian_golden.namespace_path
 #   }
 # }
+
+resource "oci_core_image" "debian_golden" {
+    #Required
+    compartment_id = var.compartment_id
+
+    #Optional
+    display_name = var.image_display_name
+    launch_mode = var.image_launch_mode
+
+    image_source_details {
+        source_type = "objectStorageTuple"
+        bucket_name = var.bucket_name
+        namespace_name = var.namespace
+        object_name = var.object_name # exported image name
+    }
+}
--- platform/terraform/variables.tf
+++ platform/terraform/variables.tf
@@ -3,7 +3,27 @@ variable compartment_id {
     default = "ocid1.compartment.oc1..aaaaaaaaq6xqdldlmtkmkpypkhsjymplonmuvbfpdqfii7ezu6b23utwqtba"
 }
 
-variable oci_namespace {
+variable namespace {
     type = string
     default = "axhocnnse6ez"
+}
+
+variable bucket_name {
+    type = string
+    default = "debian-golden"
+}
+
+variable object_name {
+    type = string
+    default = "golden.raw-disk001.vmdk"
+}
+
+variable image_display_name {
+    type = string
+    default = "debian-golden"
+}
+
+variable image_launch_mode {
+    type = string
+    default = "PARAVIRTUALIZED"
 }
