7b192fab460ad97982e924925346a5c563e902bb
Robert Starmer <rstarmer@gmail.com>
2023-05-16 02:22:29 -0700

    Minor tweaks
    - Remove unused variables
    - Update comments in run.sh
    - Comment and clean up upload-image.sh

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Get public keys for packer user from github
+# Set SSH password and build time
 SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 BUILD_TIME=$(date +%Y-%m-%d-%H-%M-%S)
+# Pass SSH password and build time to packer
 PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} -var build_time=${BUILD_TIME} bullseye.pkr.hcl
-if [ $? -gt 0 ]; then exit 1; fi
