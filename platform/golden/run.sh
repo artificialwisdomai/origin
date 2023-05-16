ab4f4e375cd79eed70f243bc53ce3e4c3200ebbc
Robert Starmer <rstarmer@gmail.com>
2023-05-16 12:27:27 -0700

    Update resources to remove passwords from the system.

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# Set SSH password and build time
-SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-BUILD_TIME=$(date +%Y-%m-%d-%H-%M-%S)
-# Pass SSH password and build time to packer
-PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} -var build_time=${BUILD_TIME} bullseye.pkr.hcl
+# If you need a password (e.g. for tty access) Set SSH password and build time
+# SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+# echo $SSH_PASSWORD > .env
+# SSH_CRYPTED_PASSWORD=$(openssl passwd -crypt "$SSH_PASSWORD")BUILD_TIME=$(date +%Y-%m-%d-%H-%M-%S)
+# Change the followin:
+# PACKER_LOG=1 packer build -var ssh_password=${SSH_CRYPTED_PASSWORD} bullseye.pkr.hcl
+
+PACKER_LOG=1 packer build bullseye.pkr.hcl
