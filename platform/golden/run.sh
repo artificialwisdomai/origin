2f10ce4e2dc38761f2e848250f5c7281e03064de
Robert Starmer <rstarmer@gmail.com>
2023-05-17 01:59:23 -0700

    still needs a password

--- platform/golden/run.sh
+++ platform/golden/run.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# If you need a password (e.g. for tty access) Set SSH password and build time
-# SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-# echo $SSH_PASSWORD > .env
-# SSH_CRYPTED_PASSWORD=$(openssl passwd -crypt "$SSH_PASSWORD")BUILD_TIME=$(date +%Y-%m-%d-%H-%M-%S)
-# Change the followin:
-# PACKER_LOG=1 packer build -var ssh_password=${SSH_CRYPTED_PASSWORD} bullseye.pkr.hcl
-
-PACKER_LOG=1 packer build bullseye.pkr.hcl
+# Set SSH password
+SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+# Pass SSH password and build time to packer
+PACKER_LOG=1 packer build -var ssh_password=${SSH_PASSWORD} bullseye.pkr.hcl
