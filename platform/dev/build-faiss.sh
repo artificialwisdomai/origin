98e75fe5ddba3b0588d15af924f364af605bb4fb
Robert Starmer <robert@computelify>
2023-07-06 00:12:43 +0000

    Used different cmake build process

--- platform/dev/build-faiss.sh
+++ platform/dev/build-faiss.sh
@@ -7,11 +7,12 @@ cd faiss
 export PATH=$PATH:$HOME/.local/bin:/usr/local/cuda/bin
 source /opt/intel/oneapi/setvars.sh
 
-export CC=gcc-12
-export CXX=g++-12
+#export CC=gcc-12
+#export CXX=g++-12
 # Configure using cmake
 
-cmake -B build . -DBUILD_SHARED_LIBS=ON -DFAISS_ENABLE_GPU=ON -DFAISS_ENABLE_PYTHON=ON -DFAISS_ENABLE_RAFT=OFF -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON -DFAISS_ENABLE_C_API=ON -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx2 -Wno-dev
+LD_LIBRARY_PATH= MKLROOT=/opt/intel/oneapi/mkl/2023.1.0/ CXX=g++-11 cmake -B build -DBUILD_TESTING=ON -DFAISS_ENABLE_GPU=OFF              -DFAISS_OPT_LEVEL=axv2              -DFAISS_ENABLE_C_API=ON              -DCMAKE_BUILD_TYPE=Release              -DBLA_VENDOR=Intel10_64_dyn -Wno-dev .
+#cmake -B build . -DBUILD_SHARED_LIBS=ON -DFAISS_ENABLE_GPU=ON -DFAISS_ENABLE_PYTHON=ON -DFAISS_ENABLE_RAFT=OFF -DBUILD_TESTING=ON -DBUILD_SHARED_LIBS=ON -DFAISS_ENABLE_C_API=ON -DCMAKE_BUILD_TYPE=Release -DFAISS_OPT_LEVEL=avx2 -Wno-dev
 
 # Now build faiss
 
