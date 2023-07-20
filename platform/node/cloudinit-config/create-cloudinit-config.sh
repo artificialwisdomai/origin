dbb17296414b921eeba1ebc0d33a231d5d96bfa8
Steven Dake <steve@computelify.com>
2023-07-20 04:38:23 -0700

    Creates a per-VM cloudinit configuration

--- platform/node/cloudinit-config/create-cloudinit-config.sh
+++ platform/node/cloudinit-config/create-cloudinit-config.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo rm -f $HOME/cloudinit-config-${1}.img
-sudo mkdosfs -n CIDATA -C $HOME/cloudinit-config-${1}.img 8192
-sudo chown -R $USER:$USER $HOME/cloudinit-config-${1}.img
+rm -f $HOME/cloudinit-config-${1}.img
+/usr/sbin/mkdosfs -n CIDATA -C $HOME/cloudinit-config-${1}.img 8192
 mcopy -oi $HOME/cloudinit-config-${1}.img -s ${1}/user-data ::
 mcopy -oi $HOME/cloudinit-config-${1}.img -s ${1}/meta-data ::
-
+sudo cp $HOME/cloudinit-config-${1}.img /var/lib/artificial_wisdom/
