913b909fdf132d0c75d73e4d06d4c2ff4af94d36
Steven Dake <steve@computelify.com>
2023-08-17 00:32:29 -0700

    Introduce a bookworm image
    We likely don't need bullseye going forward. My suggestion is to
    maintain the most leading edge version of an image. As updated
    images are produced, older images, containing their full workload,
    will be recycled into a newer image.

---
+++ platform/golden/bookworm.pkrvars.hcl
@@ -0,0 +1,6 @@
+purpose = "baseline"
+distro_version = "bookworm"
+disk_size = "200000"
+iso_url = "https://cdimage.debian.org/cdimage/release/12.1.0/amd64/iso-cd/debian-12.1.0-amd64-netinst.iso"
+iso_checksum = "sha512:9da6ae5b63a72161d0fd4480d0f090b250c4f6bf421474e4776e82eea5cb3143bf8936bf43244e438e74d581797fe87c7193bbefff19414e33932fe787b1400f"
+guest_os_type =   "Debian12_64"
---
+++ platform/golden/run-bookworm.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p ${PWD}/packer_logs
+# This SSH password remains insecure, however, its more secure then it was.
+# SSH_PASSWORD=$(cat /dev/urandom | LC_ALL=C tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+# pass an additional flag `-var ssh_password=${SSH_PASSWORD} to use.
+# to simplify development this is disabled at the moment.
+
+PACKER_LOG_PATH=${PWD}/packer_logs/log.txt PACKER_LOG=1 packer build -force -on-error=abort -var-file=bookworm.pkrvars.hcl image-cd.pkr.hcl
