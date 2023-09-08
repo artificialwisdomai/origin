24f169f9500930a250f0a9a73f86413c29b814c2
Steven Dake <steve@computelify.com>
2023-09-08 17:39:03 +0000

    Start the introduction of NVIDIA GPUs
    The RETRO workflow mandates GPUs. There is no way to run with classical compute.
    As a result, paralell (iE accelerated compute) compute is needed. GPUs are an
    Instantiation thereof.

--- workloads/retro-next/build_knowledge_base/run.sh
+++ workloads/retro-next/build_knowledge_base/run.sh
@@ -1,1 +1,12 @@
-docker buildx run artificialwisdomai/build_knowledge_base:latest
+####
+#
+# Workflow requires GPUs. There is no way to run on classical compute.
+
+# https://docs.docker.com/engine/reference/commandline/run/
+# https://github.com/NVIDIA/nvidia-docker/issues/1268#issuecomment-632692949
+#
+# TLDR;
+# sudo apt install nvidia-container-toolkit
+# sudo systemctl restart docker
+
+docker run --init --gpus all --rm artificialwisdomai/build_knowledge_base:latest
