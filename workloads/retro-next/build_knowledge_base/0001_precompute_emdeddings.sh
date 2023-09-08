ad8c68ff5c5d0373feb1155ecb27f79c5f7ae636
Steven Dake <steve@computelify.com>
2023-09-08 16:56:27 +0000

    Imporve the workflow by adding variables for important paths

--- workloads/retro-next/build_knowledge_base/0001_precompute_emdeddings.sh
+++ workloads/retro-next/build_knowledge_base/0001_precompute_emdeddings.sh
@@ -3,8 +3,9 @@
 # step 1. precompute embeddings for all chunks
 #
 # outputs:
-# $HOME/retro/data/index/chunks.embeddings.npy
+# ${INDEX_PATH}/chunks.embeddings.npy
+# ${INDEX_PATH}/chunks.npy
 
-python $HOME/repos/retro/src/data/embed_chunks.py \
-	$HOME/repos/retro/data/index/chunks.npy \
-	$HOME/repos/retro/data/index/chunks.embeddings.npy
+python ${SRC_PATH}/embed_chunks.py \
+	${INDEX_PATH}/chunks.npy \
+	${INDEX_PATH}/chunks.embeddings.npy
--- workloads/retro-next/build_knowledge_base/0003_train_index.sh
+++ workloads/retro-next/build_knowledge_base/0003_train_index.sh
@@ -3,11 +3,11 @@
 # Step 4. Train a faiss index
 #
 # outputs:
-# $HOME/retro/data/IVF131072,PQ32.index
+# ${INDEX_PATH}/IVF131072,PQ32.index
 
-python $HOME/repos/retro/src/data/train_faiss_index.py \
-        --spec $HOME/repos/retro/data/index/index-spec.json \
+python ${SRC_HOME}/train_faiss_index.py \
+        --spec ${INDEX_PATH}/index-spec.json \
         --max-training-vectors 33554432 \
         --index-type IVF131072,PQ32 \
-        --output $HOME/repos/retro/data/IVF131072,PQ32.index \
+        --output ${INDEX_PATH}/IVF131072,PQ32.index \
         --use-gpus
