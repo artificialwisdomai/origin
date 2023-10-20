3ef1ea9d15e3d81e5fdcf659f89c681f77f8cbf2
Corbin <cds@corbinsimpson.com>
2023-10-20 10:46:01 -0700

    Write a couple short commands for uploading and downloading with OCI.

--- platform/wisdomctl/wisdomctl.py
+++ platform/wisdomctl/wisdomctl.py
@@ -1,2 +1,31 @@
-def main():
-    print(42)
+from ocifs import OCIFileSystem
+
+import click
+
+@click.group()
+def cli(): pass
+
+REMOTE = click.argument("remote")
+
+MB = 1024 ** 2
+def slowCopy(src, dest):
+    while True:
+        hunk = src.read(MB)
+        if not hunk: break
+        dest.write(hunk)
+
+@cli.command()
+@REMOTE
+@click.argument("local", type=click.File("wb"))
+def getFromSource(remote, local):
+    fs = OCIFileSystem(config="~/.oci/config", profile="DEFAULT")
+    with fs.open(remote, "rb") as handle: slowCopy(handle, local)
+
+@cli.command()
+@click.argument("local", type=click.File("rb"))
+@REMOTE
+def sendToSource(local, remote):
+    fs = OCIFileSystem(config="~/.oci/config", profile="DEFAULT")
+    with fs.open(remote, "wb") as handle: slowCopy(local, handle)
+
+main = cli
