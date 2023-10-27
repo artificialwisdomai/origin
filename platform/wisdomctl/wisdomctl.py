bd61ffc753bec8a599a0bb856851dc84f2da8c4d
Corbin <cds@corbinsimpson.com>
2023-10-27 10:36:27 -0700

    Write with Feather format.
    As requested, ask for zstd compression.
    
    Poetry/poetry2nix pulls a wheel with incorrect linkage for me (wrong C++
    libraries?), so I've enumerated the actual dependencies in the Nix
    flake. This means that I don't get to share exactly the same Python
    package versions between the flake and `poetry.lock`, but I'm willing to
    make that sacrifice in order to actually test my changes!
    
    Note that there is a slight asymmetry in how JSON Lines is converted to
    Feather; Apache Arrow was not designed to have full interchange with
    JSON Lines, but we can ask Pandas to make up the difference.

--- platform/wisdomctl/wisdomctl.py
+++ platform/wisdomctl/wisdomctl.py
@@ -1,3 +1,5 @@
+import pyarrow.feather, pyarrow.json
+
 from ocifs import OCIFileSystem
 
 import click
@@ -5,25 +7,25 @@ import click
 @click.group()
 def cli(): pass
 
-MB = 1024 ** 2
-def slowCopy(src, dest):
-    while True:
-        hunk = src.read(MB)
-        if not hunk: break
-        dest.write(hunk)
-
 @cli.command()
 @click.argument("remote")
 @click.argument("local", type=click.File("wb"))
 def getFromSource(remote, local):
     fs = OCIFileSystem(config="~/.oci/config", profile="DEFAULT")
-    with fs.open(remote, "rb") as handle: slowCopy(handle, local)
+    with fs.open(remote, "rb") as handle:
+        df = pyarrow.feather.read_feather(handle)
+        # DataFrame -> JSON Lines: https://stackoverflow.com/a/45570174/264985
+        df.to_json(local, orient="records", lines=True)
 
 @cli.command()
 @click.argument("local", type=click.File("rb"))
 @click.argument("remote")
 def sendToSource(local, remote):
     fs = OCIFileSystem(config="~/.oci/config", profile="DEFAULT")
-    with fs.open(remote, "wb") as handle: slowCopy(local, handle)
+    with fs.open(remote, "wb") as handle:
+        df = pyarrow.json.read_json(local)
+        pyarrow.feather.write_feather(df, handle, compression="zstd")
 
 main = cli
+
+if __name__ == "__main__": raise SystemExit(main())
