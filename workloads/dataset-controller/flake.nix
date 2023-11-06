dd68df041b5ec8f1961abc7ea791dd9fc372380c
Corbin <cds@corbinsimpson.com>
2023-11-06 12:31:49 -0800

    Stub out a basic lookup to HF Datasets.

--- workloads/dataset-controller/flake.nix
+++ workloads/dataset-controller/flake.nix
@@ -8,6 +8,17 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+	# Need a recent HF Datasets release due to a buggy interaction with
+	# newer fsspec
+        datasets = pkgs.python310Packages.datasets.overrideAttrs (_: _: rec {
+          version = "2.14.6";
+          src = pkgs.fetchFromGitHub {
+            owner = "huggingface";
+            repo = "datasets";
+            rev = version;
+            sha256 = "sha256-AncyuDiBNPVleSVsxwsJ27SJzXsvQYAVq8DOOG40rP4=";
+          };
+        });
         py = pkgs.python310.withPackages (ps: with ps; [
           # Cloud-native: Access to k8s API, M&M
           kubernetes prometheus_client
