8f94f8b15d2fb7ea3b16b4cd02739db623b1a069
Corbin <cds@corbinsimpson.com>
2023-11-06 17:20:53 -0800

    Flesh out basic dataset-uploading.

--- workloads/dataset-controller/README.md
+++ workloads/dataset-controller/README.md
@@ -7,6 +7,14 @@ them to OCI in Feather format. (Or at least, that's the plan.)
 
 A Dockerfile is included for convenience.
 
+=== OCI Configuration ===
+
+To access OCI, bind-mount a configuration file. For example, to bind-mount
+`~/.oci/config`, try something like:
+
+    export OCI_CONFIG=/app/oci/config
+    podman -e OCI_CONFIG -v ~/.oci:/app/oci:ro ...
+
 === Rancher/k3s ===
 
 Make sure to export `KUBECONFIG`:
@@ -15,4 +23,11 @@ Make sure to export `KUBECONFIG`:
 
 Then also add it to the container environment:
 
-    podman run -E KUBECONFIG ...
+    podman run -e KUBECONFIG ...
+
+=== Complete example ====
+
+    export IMAGE=$(podman build .)
+    export KUBECONFIG=/app/kube/k3s.yaml
+    export OCI_CONFIG=/app/oci
+    podman run --env-host -v /etc/rancher/k3s:/app/kube:ro -v ~/.oci:/app/oci:ro --net host $IMAGE
--- workloads/dataset-controller/dataset-controller.py
+++ workloads/dataset-controller/dataset-controller.py
@@ -1,29 +1,52 @@
 import os
 
 from kubernetes import config, client, watch
+from prometheus_client import Counter, start_http_server
 
+from ocifs import OCIFileSystem
+
+import pyarrow.feather
 from datasets import load_dataset
 
 DOMAIN = "new.artificialwisdom.cloud"
 RESOURCE_VERSION = ""
+
+MAIN_LOOP_ITERATIONS = Counter("main_loop_iterations",
+                               "Number of times main loop has restarted")
+OBJECTS_PROCESSED = Counter("objects_processed",
+                            "Number of objects processed")
+
+fs = OCIFileSystem(config=os.environ["OCI_CONFIG"], profile="DEFAULT")
 
 if "KUBERNETES_PORT" in os.environ:
     config.load_incluster_config()
 else:
     config.load_kube_config()
 
+start_http_server(8000)
+
 crds = client.CustomObjectsApi()
-for i in range(10):
-    print("main loop")
+while True:
+    MAIN_LOOP_ITERATIONS.inc()
     stream = watch.Watch().stream(
             crds.list_cluster_custom_object,
             DOMAIN, "v1",
             "datasets", resource_version=RESOURCE_VERSION)
     for event in stream:
+        OBJECTS_PROCESSED.inc()
         obj = event["object"]
-        print("Got obj", obj)
         source = obj["spec"]["source"]
-        print("source", source)
         if source.startswith("hf:"):
-            ds = load_dataset(source[3:])
-            print("got dataset", ds)
+            name = source[3:]
+            print("Retrieving dataset from HF", name)
+            ds = load_dataset(name)["train"]
+            df = ds.to_pandas()
+            filename = obj["spec"]["filename"]
+            bucket = obj["spec"]["bucket"]
+            remote = f"oci://{bucket}/{filename}"
+            print("Copying dataset to", remote)
+            with fs.open(remote, "wb") as handle:
+                pyarrow.feather.write_feather(df, handle, compression="zstd")
+            print("Upload successful!")
+        else:
+            print("Not sure what to do with source", source)
