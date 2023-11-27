06777081a3f7e6fef5955b2f963ff3dbc3a061ad
Steven Dake <steve@computelify.com>
2023-11-27 18:49:41 +0000

    introducing virtiofsd

---
+++ platform/Dockerfiles/virtiofsd/build.sh
@@ -0,0 +1,3 @@
+date=$(date '+%Y%m%d%H%M%S')
+
+docker buildx build -t artificialwisdomai/virtiofsd:v1.8.0-${date} -t artificialwisdomai/cloud-hypervisor:v1.8.0 --output type=local,dest="${PWD}/target" --progress plain .
