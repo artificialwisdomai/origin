2acd62fd165d025e0182580f5dfc774ee551f540
Steven Dake <steve@computelify.com>
2023-12-02 17:36:10 +0000

    Refine the faiss build

--- platform/packaging/build/faiss/build.sh
+++ platform/packaging/build/faiss/build.sh
@@ -1,4 +1,3 @@
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build -t artificialwisdomai/faiss:v1.7.4-${date} -t artificialwisdomai/faiss:v1.7.4 --output type=local,dest="${PWD}/target" .
-
+docker buildx build -t artificialwisdomai/faiss:v1.7.4-${date} -t artificialwisdomai/faiss:v1.7.4 --output type=local,dest="${PWD}/target" --progress plain .
