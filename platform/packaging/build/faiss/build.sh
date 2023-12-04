68e3f92660c421b34a6eba9524c09490c08cb391
Steven Dake <steve@computelify.com>
2023-12-04 20:34:58 +0000

    More improvements for faiss.

--- platform/packaging/build/faiss/build.sh
+++ platform/packaging/build/faiss/build.sh
@@ -1,3 +1,3 @@
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build --tag artificialwisdomai/faiss:v1.7.4-${date} --tag artificialwisdomai/faiss:v1.7.4 --output type=local,dest="${PWD}/target" --no-cache --progress plain .
+docker buildx build --tag artificialwisdomai/faiss:v1.7.4-${date} --tag artificialwisdomai/faiss:v1.7.4 --output type=local,dest="${PWD}/target" --progress plain .
