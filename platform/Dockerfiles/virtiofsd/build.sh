68bd0faf71e193a03f6821072dec2d53527541e9
Steven Dake <steve@computelify.com>
2024-05-04 07:14:10 -0700

    Update virtiofsd versions

--- platform/Dockerfiles/virtiofsd/build.sh
+++ platform/Dockerfiles/virtiofsd/build.sh
@@ -1,3 +1,3 @@
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build -t artificialwisdomai/virtiofsd:v1.8.0-${date} -t artificialwisdomai/cloud-hypervisor:v1.8.0 --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --progress=plain -t artificialwisdomai/virtiofsd:v1.8.0-${date} -t artificialwisdomai/cloud-hypervisor:v1.8.0 --output type=local,dest="${PWD}/target" --progress plain .
