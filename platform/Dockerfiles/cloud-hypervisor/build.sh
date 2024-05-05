4fabf0338dcf69dbfe8e6b024a6d2193e60786da
Steven Dake <steve@computelify.com>
2024-05-04 22:38:23 -0700

    Tag build properly

--- platform/Dockerfiles/cloud-hypervisor/build.sh
+++ platform/Dockerfiles/cloud-hypervisor/build.sh
@@ -2,4 +2,3 @@ date=$(date '+%Y%m%d%H%M%S')
 
 docker buildx build --tag artificialwisdomai/cloud-hypervisor:v39.0-${date} --output type=docker --progress plain .
 docker buildx build --tag artificialwisdomai/cloud-hypervisor:v39.0-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --tag artificialwisdomai/cloud-hypervisor:v39.0 --output type=docker --progress plain .
