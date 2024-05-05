d1b8a882c1e23fd3524e9fcff14332f7dac4b3b6
Steven Dake <steve@computelify.com>
2024-05-04 22:11:38 -0700

    Build cloud-hypervisor with specific version tag

--- platform/Dockerfiles/cloud-hypervisor/build.sh
+++ platform/Dockerfiles/cloud-hypervisor/build.sh
@@ -1,4 +1,6 @@
+CLOUD_HYPERVISOR_VERSION=v39.0
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build --tag artificialwisdomai/cloud-hypervisor:v39.0-${date} --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/cloud-hypervisor:v39.0-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --tag artificialwisdomai/cloud-hypervisor:${CLOUD_HYPERVISOR_VERSION}-${date} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/cloud-hypervisor:${CLOUD_HYPERVISOR_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --tag artificialwisdomai/cloud-hypervisor:${CLOUD_HYPERVISOR_VERSION} --output type=docker --progress plain .
