71fdd2f63dcc8718e1ec727b6a3b0ffdb2a31ce4
Steven Dake <steve@computelify.com>
2024-05-04 22:38:23 -0700

    Tag build properly

--- platform/Dockerfiles/virtiofsd/build.sh
+++ platform/Dockerfiles/virtiofsd/build.sh
@@ -1,3 +1,6 @@
 date=$(date '+%Y%m%d%H%M%S')
+VIRTIOFSD_VERSION=v1.10.1
 
-docker buildx build --progress=plain -t artificialwisdomai/virtiofsd:v1.8.0-${date} -t artificialwisdomai/cloud-hypervisor:v1.8.0 --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:${VIRTIOFSD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:${VIRTIOFSD_VERSION}-${date} --output type=docker --progress plain .
+docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:${VIRTIOFSD_VERSION} --output type=docker --progress plain .
