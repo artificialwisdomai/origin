c160504ff0ec3a850cc1fa523d1ed0f81c7c5886
Steven Dake <steve@computelify.com>
2024-05-08 17:18:42 -0700

    Remoe duplicate faiss

--- platform/packaging/build/faiss/build.sh
+++
@@ -1,3 +0,0 @@
-date=$(date '+%Y%m%d%H%M%S')
-
-docker buildx build --tag artificialwisdomai/faiss:v1.7.4.0-${date} --tag artificialwisdomai/faiss:v1.7.4.0 --output type=local,dest="${PWD}/target" --progress plain .
