53a8b2b60622c48d4b17aad298934ab13dc751e3
Steven Dake <steve@computelify.com>
2024-08-03 16:38:51 -0700

    Update virtiofsd
    Simplify by removing comments which are in github
    Simplify by using long options instead of short options
    Simplify by
    Update rust to v1.80
    Update virtiofsd to v11.1
    Save binary and deb files
    Use cargo-deb to build virtiofsd and save debian files

--- platform/Dockerfiles/virtiofsd/build.sh
+++ platform/Dockerfiles/virtiofsd/build.sh
@@ -1,6 +1,6 @@
 date=$(date '+%Y%m%d%H%M%S')
-VIRTIOFSD_VERSION=v1.10.1
+VIRTIOFSD_VERSION=1.11.1
 
-docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:${VIRTIOFSD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:${VIRTIOFSD_VERSION}-${date} --output type=docker --progress plain .
-docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:${VIRTIOFSD_VERSION} --output type=docker --progress plain .
+docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION}-${date} --output type=docker --progress plain .
+docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION} --output type=docker --progress plain .
