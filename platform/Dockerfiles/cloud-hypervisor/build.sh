7281ef4baa67af5d8bdd5f335e67c44b3030df98
Steven Dake <steve@computelify.com>
2024-08-03 15:37:57 -0700

    Update cloud-hypevisor
    Revise rust from 1.78 to 1.80
    Simplify build by removing rustc execution
    Revise cloud-hypervisor from v39 to v40
    Output debian packages with cargo-deb

--- platform/Dockerfiles/cloud-hypervisor/build.sh
+++ platform/Dockerfiles/cloud-hypervisor/build.sh
@@ -1,6 +1,6 @@
-CLOUD_HYPERVISOR_VERSION=v39.0
+CLOUD_HYPERVISOR_VERSION=40.0
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build --tag artificialwisdomai/cloud-hypervisor:${CLOUD_HYPERVISOR_VERSION}-${date} --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/cloud-hypervisor:${CLOUD_HYPERVISOR_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --tag artificialwisdomai/cloud-hypervisor:${CLOUD_HYPERVISOR_VERSION} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/cloud-hypervisor:v${CLOUD_HYPERVISOR_VERSION} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/cloud-hypervisor:v${CLOUD_HYPERVISOR_VERSION}-${date} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/cloud-hypervisor:v${CLOUD_HYPERVISOR_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
