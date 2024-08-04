53a03e17b95da3e3dcce1e2c7b5e43b5077a387a
Steven Dake <steve@computelify.com>
2024-08-03 21:15:13 -0700

    More cleanup within faiss
    - Download mkl from Intel's apt repository
    - Save tarball of mkl (/opt/facebook)
    - Save tarball of cuda (/opt/nvidia)
    - Version cleanups for consistency

--- platform/Dockerfiles/faiss/build.sh
+++ platform/Dockerfiles/faiss/build.sh
@@ -1,3 +1,6 @@
 date=$(date '+%Y%m%d%H%M%S')
+FAISS_VERSION=1.8.0
 
-docker buildx build --tag artificialwisdomai/faiss:v1.7.4.0-${date} --tag artificialwisdomai/faiss:v1.7.4.0 --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0 --tag --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0-${date} --output type=docker --progress plain .
