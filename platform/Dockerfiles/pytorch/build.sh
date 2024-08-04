134071a1bae0b5bac13a167583a2382a8a9a4927
Steven Dake <steve@computelify.com>
2024-08-04 03:57:40 -0700

    Tidy pytorch and add image labels

--- platform/Dockerfiles/pytorch/build.sh
+++ platform/Dockerfiles/pytorch/build.sh
@@ -2,5 +2,10 @@
 #
 # Build pytorch and output the build results to  "${PWD}/target"
 
-mkdir -p "${PWD}/target"
-docker buildx build --progress plain --output type=local,dest="${PWD}/target" . -t pytorch:v2.3.1
+date=$(date '+%Y%m%d%H%M%S')
+PYTORCH_VERSION=2.4.0
+PYTORCH_EPOCH=0
+
+docker buildx build --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH}-${date} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH}-${date} --output type=local,dest="${PWD}/target" --progress plain .
