9378d0cb0e56bd64872fd97eed3bcea9698585fd
Steven Dake <steve@computelify.com>
2024-08-10 16:44:13 -0700

    Name the package rust-hypervisor-fw

--- platform/Dockerfiles/rust-hypervisor-firmware/build.sh
+++ platform/Dockerfiles/rust-hypervisor-firmware/build.sh
@@ -1,7 +1,7 @@
-RUST_HYPERVISOR_FIRMWARE_VERSION=v0.4.2
-RUST_HYPERVISOR_FIRMWARE_EPOCH=0
+RUST_HYPERVISOR_FW_VERSION=v0.4.2
+RUST_HYPERVISOR_FW_EPOCH=0
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build --tag artificialwisdomai/rust-hypervisor-version:${RUST_HYPERVISOR_FIRMWARE_VERSION}-${RUST_HYPERVISOR_FIRMWARE_EPOCH}-${date} --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/rust-hypervisor-version:${RUST_HYPERVISOR_FIRMWARE_VERSION}-${RUST_HYPERVISOR_FIRMWARE_EPOCH}-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --tag artificialwisdomai/crust-hypervisor-version:${RUST_HYPERVISOR_FIRMWARE_VERSION}-${RUST_HYPERVISOR_FIRMWARE_EPOCH} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/rust-hypervisor-firmware:${RUST_HYPERVISOR_FW_VERSION}-${RUST_HYPERVISOR_FW_EPOCH}-${date} --output type=docker --progress plain .
+docker buildx build --tag artificialwisdomai/rust-hypervisor-firmware:${RUST_HYPERVISOR_FW_VERSION}-${RUST_HYPERVISOR_FW_EPOCH}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --tag artificialwisdomai/rust-hypervisor-firmware:${RUST_HYPERVISOR_FW_VERSION}-${RUST_HYPERVISOR_FW_EPOCH} --output type=docker --progress plain .
