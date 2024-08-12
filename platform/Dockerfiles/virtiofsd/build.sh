e4998678cd38edc5f565de85d6590d6a04d17f82
Steven Dake <steve@computelify.com>
2024-08-12 14:34:43 -0700

    Use base toolchain image when building virtiofsd

--- platform/Dockerfiles/virtiofsd/build.sh
+++ platform/Dockerfiles/virtiofsd/build.sh
@@ -1,6 +1,7 @@
 date=$(date '+%Y%m%d%H%M%S')
 VIRTIOFSD_VERSION=1.11.1
+TOOLCHAIN_REVISION=20240812
 
-docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION}-${date} --output type=docker --progress plain .
-docker buildx build --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION}-${date} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --progress=plain --tag artificialwisdomai/virtiofsd:v${VIRTIOFSD_VERSION} --output type=docker --progress plain .
