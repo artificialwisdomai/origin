a19cece7dd66bf89a5718201680758ebac29b910
Steven Dake <steve@computelify.com>
2024-08-13 05:45:12 +0000

    Build istiod deb package

--- platform/Dockerfiles/istiod/build.sh
+++ platform/Dockerfiles/istiod/build.sh
@@ -1,6 +1,7 @@
 ISTIOD_VERSION=v1.22.5
+TOOLCHAIN_REVISION=20240813
 date=$(date '+%Y%m%d%H%M%S')
 
-docker buildx build --tag artificialwisdomai/istiod:${ISTIOD_VERSION}-${date} --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/istiod:${ISTIOD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --tag artificialwisdomai/istiod:${ISTIOD_VERSION} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/istiod:${ISTIOD_VERSION}-${date} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/istiod:${ISTIOD_VERSION}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/istiod:${ISTIOD_VERSION} --output type=docker --progress plain .
