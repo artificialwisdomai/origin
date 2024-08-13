e2b5ac7bcf3c3a208f7d60322f6e9c1af531fdcf
Steven Dake <steve@computelify.com>
2024-08-13 03:44:42 +0000

    Make faiss work with toolchain base image

--- platform/Dockerfiles/faiss/build.sh
+++ platform/Dockerfiles/faiss/build.sh
@@ -1,6 +1,7 @@
 date=$(date '+%Y%m%d%H%M%S')
 FAISS_VERSION=1.8.0
+TOOLCHAIN_REVISION=20240813
 
-docker buildx build --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0-${date} --output type=local,dest="${PWD}/target" --progress plain .
-docker buildx build --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0 --tag --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0-${date} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0 --tag --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/faiss:v${FAISS_VERSION}.0-${date} --output type=docker --progress plain .
