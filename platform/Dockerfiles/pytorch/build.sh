607e86c99a7b7e40629c0b42a145b245285b229e
Steven Dake <steve@computelify.com>
2024-08-13 05:36:36 +0000

    Update pytorch to use toolchain

--- platform/Dockerfiles/pytorch/build.sh
+++ platform/Dockerfiles/pytorch/build.sh
@@ -2,10 +2,11 @@
 #
 # Build pytorch and output the build results to  "${PWD}/target"
 
+TOOLCHAIN_REVISION=20240813
 date=$(date '+%Y%m%d%H%M%S')
 PYTORCH_VERSION=2.4.0
 PYTORCH_EPOCH=0
 
-docker buildx build --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH} --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH}-${date} --output type=docker --progress plain .
-docker buildx build --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH}-${date} --output type=local,dest="${PWD}/target" --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH}-${date} --output type=docker --progress plain .
+docker buildx build --build-arg TOOLCHAIN_REVISION=${TOOLCHAIN_REVISION} --tag artificialwisdomai/pytorch:v${PYTORCH_VERSION}_${PYTORCH_EPOCH}-${date} --output type=local,dest="${PWD}/target" --progress plain .
